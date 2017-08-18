1203e62def9f9e7249ab641860b5b9a620840c79
Edmond <edipetres@gmail.com>
2017-08-18 13:01:23 +0200

    close #18 add basic test cases

--- src/index.test.js
+++ src/index.test.js
@@ -3,17 +3,12 @@ import { expect } from 'chai';
 import cheerio from 'cheerio';
 import fs from 'fs';
 
-describe('My first test', () => {
-  it('Should pass', () => {
-    expect(true).to.equal(true);
-  });
-});
 
 describe('index.html', () => {
-  it('Should say hello', (done) => {
+  it('Should have Praqma in the header', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     const $ = cheerio.load(index);
-    expect($("h1").text()).to.equal('Users');
+    expect($("#navbar-top-brand").text()).to.equal('Praqma')
     done();
   });
   /*it('should get users', (done) => {
