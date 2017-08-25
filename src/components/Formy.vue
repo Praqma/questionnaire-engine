8737b73c719e2839934976aad42826f10d92f90c
Edmond <edipetres@gmail.com>
2017-08-25 10:20:11 +0200

    close #21 add vue popup for forms

--- src/components/Formy.vue
+++ src/components/Formy.vue
@@ -1,8 +1,18 @@
 <template>
+  <!-- <div>
+      <div v-for="(item, index) in questions" :key="index">
+        <h4>{{item.ask}}</h4>
+        <p>{{item.description}}</p>
+      </div>
+    </div> -->
   <div>
-    <div v-for="(item, index) in questions" :key="index">
-      <h4>{{item.ask}}</h4>
-      <p>{{item.description}}</p>
+    <button class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          Questionaire comes below
+        </div>
+      </div>
     </div>
   </div>
 </template>
