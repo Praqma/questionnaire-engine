d83b0caf7b585efe83a2d3675cc4f94cb1056721
Edmond <edipetres@gmail.com>
2017-09-04 15:27:41 +0200

    load data dirs recursively into array of json objects

--- api/data-controller.js
+++ api/data-controller.js
@@ -1,56 +1,93 @@
 import fs from 'fs'
 import yamljs from 'yamljs'
 import path from 'path'
-import { dataDir } from './config'
+// import { dataDir } from './config'
 
-let base = process.env.PWD;
+let basePath = process.env.PWD;
+let dataDir = "/content_data"
 
 // FEAT: for yaml schema validation use Kwalify
 
-export function getAllQuestionnaire(callback) {
-  let res = walk(path.join(base, "/content_data"))
-  console.log(res);
+export function getAllQuestionnaire() {
+  let questionnaireDirs = getDirsWithLayoutFile(path.join(basePath, "/content_data"))
+  let allData = questionnaireDirs.map(function(dir){
+    // console.log("  looking for dir --> " + dir);
+
+    getAllDataInDir(dir, function(err, resp){
+      console.log('   resp ->> ' + resp);
+    });
+  })
+
+  console.log(allData);
 
 
 }
 
-var walk = function(dir) {
-  var results = []
+function getDirsWithLayoutFile(dir) {
+  var dirsWithConfigFile = []
+  // var results = []
   var list = fs.readdirSync(dir)
-  list.forEach(function(file) {
-      file = dir + '/' + file
-      var stat = fs.statSync(file)
-      if (stat && stat.isDirectory()) results = results.concat(walk(file))
-      else results.push(file)
+  list.forEach(function (file) {
+    if (file === "Layout.yml") {
+      dirsWithConfigFile.push(dir)
+    }
+
+    // file = dir + '/' + file
+    file = path.join(dir, file)
+    var stat = fs.statSync(file)
+
+    // do recursive search through subfolders
+    if (stat && stat.isDirectory())
+      dirsWithConfigFile = dirsWithConfigFile.concat(getDirsWithLayoutFile(file))
   })
-  return results
+  return dirsWithConfigFile
 }
 
-// get all yaml files in data dir and return converted json array of the content
-export function getAllData(callback) {
+function getAllFilesInDir(dir, fileList) {
+  fileList = fileList || [];
+
+  var files = fs.readdirSync(dir);
+  for (var i in files) {
+    if (!files.hasOwnProperty(i))
+      continue;
+    var name = dir + '/' + files[i];
+    if (fs.statSync(name).isDirectory()) {
+      getAllFilesInDir(name, fileList);
+    } else {
+      fileList.push(name);
+    }
+  }
+  return fileList;
+}
+
+// returns an array of JSON object questionnaires given the absolute path to a directory
+export function getAllDataInDir(dataDir, callback) {
   let responseArray = []
+  let items = getAllFilesInDir(dataDir);
 
-  fs.readdir(path.join(__dirname, dataDir), function (err, items) {
-    if (err) callback(err);
-
-    let processedItems = 0;
-    items.forEach(function (itemName) {
+  let processedItems = 0
+  items.forEach(function (item) {
+    let itemName = item.split('/').slice(-1)[0];
+    if (itemName !== "Layout.yml" && itemName !== "layout.yml") {
       try {
-        fs.readFile(path.join(__dirname, dataDir, itemName), function (err, data) {
-            if (err) {
-              callback(err)
-            }
-            let yamlString = data.toString();
-            let doc = yamljs.parse(yamlString)
-            responseArray.push(doc)
-            processedItems++;
-            if (processedItems === items.length) {
-              callback(null, responseArray)
-            }
-          })
+        fs.readFile(item, function (err, data) {
+          if (err) {
+            return callback(err)
+          }
+          let yamlString = data.toString();
+          let doc = yamljs.parse(yamlString)
+          responseArray.push(doc)
+          processedItems++;
+
+          if (processedItems === items.length - 1) {
+            return callback(null, responseArray)
+          }
+        })
+
       } catch (e) {
-        callback(e)
+        return callback(e)
       }
-    })
+    }
   })
+
 }
