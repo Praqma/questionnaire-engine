c08bc1a8c8e4212201c6f7eb3d4ab7042cf5a2df
Edmond <edipetres@gmail.com>
2017-09-25 11:00:23 +0200

    close #66 external links should open op in new tabs

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,7 +11,7 @@
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <button class="btn btn-outline-success" v-show="didOpenIntro" data-toggle="modal" data-target="#intro-form" @click="onIntroOpen" type="button"> {{ respData.intro_form.title }} </button>
-          <a class="nav-item nav-link" v-for="(link, index) in respData.links" :key="index" v-bind:href="link.url"> {{link.title}} </a>
+          <a class="nav-item nav-link" v-for="(link, index) in respData.links" :key="index" v-bind:href="link.url" target="_blank"> {{link.title}} </a>
         </div>
       </div>
     </nav>
