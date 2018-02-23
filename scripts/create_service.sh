2acee43b98efd253b87ba48f659aef754c1c0c42
Edmond <edipetres@gmail.com>
2018-02-23 11:29:58 +0100

    #164 - fix destroy_service; fix grep regex

--- scripts/create_service.sh
+++ scripts/create_service.sh
@@ -2,6 +2,8 @@
 # source /scripts/create_service.sh   <service-name>  <image-tag>                       <environment>   <db-uri>
 # source /scripts/create_service.sh   aws-qe-deploy   praqma/questionaire-engine:0.1.0  test            "mongodb://praqmadb:..."
 
+# ATTENTION: the grep command requires the -P flag for regex to work on this CI server - might throw error on differenc linux versions
+
 SERVICE_NAME=$1
 IMAGE_TAG=$2
 ENV=$3
@@ -14,11 +16,11 @@ create_service() {
 }
 
 check_service_status() {
-	fargate service info $SERVICE_NAME | grep -o 'Running: \d' | grep -o '\d'
+	fargate service info $SERVICE_NAME | grep -oP 'Running: \d' | grep -oP '\d'
 }
 
 get_ip() {
-  fargate service info $SERVICE_NAME | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
+  fargate service info $SERVICE_NAME | grep -oP '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
 }
 
 SECONDS=0
@@ -29,10 +31,13 @@ wait_for_ip() {
 
     if [ "$RUNNING" == "1" ]; then
       echo "Server is running on IP $(get_ip)"
-    else
+      break
+    elif [ "$RUNNING" == "0" ]; then
       echo "[i] Server is pending. Will refresh in 5 seconds..."
-      sleep 5s
+    else
+      echo "[!] Could not check service status."
     fi
+    sleep 5s
 
     # time out after 5 minutes (300 seconds)
     if [ $SECONDS -gt "300" ]
